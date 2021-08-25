05b4ca9da64f3b12a0cde5fdc51514006948b587
Toan Pham <toan.chpham@gmail.com>
2021-08-25 05:18:12 -0500

    add get route

--- NODEJS/EXPRESSJS/MONGOOSE/index.js
+++ NODEJS/EXPRESSJS/MONGOOSE/index.js
@@ -17,16 +17,27 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
 
 app.get("/products", async function(req, res){
     const products = await Product.find({})
     res.render("products/index.ejs", {products})
 })
 
+app.get("/products/new", function(req, res){
+    res.render("products/new.ejs")
+})
+
 app.get("/products/:id", async function(req, res){
     const {id} = req.params
     const product = await Product.findById(id)
     res.render("products/detail.ejs", {product})
+})
+
+app.post("/products", async function(req, res){
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.redirect(`/products/${newProduct._id}`)
 })
 
 app.listen(3000, function(){
